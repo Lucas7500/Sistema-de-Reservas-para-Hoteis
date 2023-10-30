d2fbc30be720d4dfa4476b59eeb07f50b5085907
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-10-30 12:17:51 -0300

    first commit

---
+++ Sistema-de-Reservas-para-Hoteis/Form1.cs
@@ -0,0 +1,10 @@
+namespace Sistema_de_Reservas_para_Hoteis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
