0fa8c25004634b1850ba1010788a6ee0126ac54f
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-10-31 12:18:44 -0300

    Implementacao do modelo de dados

---
+++ Sistema-de-Reservas-para-Hoteis/Reserva.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Reservas_para_Hoteis
+{
+    public class Reserva
+    {
+        private string Cpf { get; set; } = string.Empty;
+        private string ClientName { get; set; } = string.Empty;
+        private int Age { get; set; }
+        private string PhoneNumber { get; set; } = string.Empty;
+        private enum Gender
+        {
+            Male,
+            Female
+        }
+        private DateTime CheckIn { get; set; }
+        private DateTime CheckOut { get; set; }
+        private decimal StayPrice { get; set; }
+        private bool IsRoomAvailable { get; set; } = true;
+    }
+}
