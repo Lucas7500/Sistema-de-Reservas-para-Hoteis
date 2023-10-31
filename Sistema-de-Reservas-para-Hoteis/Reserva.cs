ad3eed13a2237cfb74d4d2cf0d17fc5f409a0f0c
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-10-31 13:14:08 -0300

    mudando tipo do Id de string para int

--- Sistema-de-Reservas-para-Hoteis/Reserva.cs
+++ Sistema-de-Reservas-para-Hoteis/Reserva.cs
@@ -8,7 +8,7 @@ namespace Sistema_de_Reservas_para_Hoteis
 {
     public class Reserva
     {
-        private string Id { get; set; } = string.Empty;
+        private int Id { get; set; }
         private string Cpf { get; set; } = string.Empty;
         private string Nome { get; set; } = string.Empty;
         private int Idade { get; set; }
