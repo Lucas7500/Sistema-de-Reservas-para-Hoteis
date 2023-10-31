6945cecba7d85de5909b0c2035452821b5d6268e
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-10-31 11:00:52 -0300

    declaring variables

--- Sistema-de-Reservas-para-Hoteis/Form1.cs
+++ Sistema-de-Reservas-para-Hoteis/Form1.cs
@@ -6,5 +6,20 @@ namespace Sistema_de_Reservas_para_Hoteis
         {
             InitializeComponent();
         }
+
+        private string Cpf { get; set; } = string.Empty;
+        private string ClientName { get; set; } = string.Empty;
+        private int Age { get; set; }
+        private string PhoneNumber { get; set; } = string.Empty;
+        private enum Gender
+        {
+            Male,
+            Female
+        }
+        private DateTime CheckIn { get; set; }
+        private DateTime CheckOut { get; set; }
+       private decimal StayPrice {  get; set; }
+       private bool IsRoomAvailable { get; set; }
+
     }
 }
