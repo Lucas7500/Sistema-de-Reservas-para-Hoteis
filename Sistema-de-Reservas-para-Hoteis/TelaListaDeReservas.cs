88345176b3f58095547b92c34a4723510a83f869
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-11-16 13:26:44 -0300

    termino da remoção, refatoração final e organização do código

--- Sistema-de-Reservas-para-Hoteis/TelaListaDeReservas.cs
+++ Sistema-de-Reservas-para-Hoteis/TelaListaDeReservas.cs
@@ -14,8 +14,8 @@ namespace Sistema_de_Reservas_para_Hoteis
         static int id = 0;
         const int primeiroElemento = 0;
         const int umaLinhaSelecionada = 1;
-        const int nenhumaLinhaSelecionada = 0;
         const int idNulo = 0;
+        const int listaNula = 0;
 
         public static void AdicionarReservaNaLista(Reserva reserva)
         {
@@ -31,11 +31,51 @@ namespace Sistema_de_Reservas_para_Hoteis
                 MessageBox.Show("A reserva foi editada com sucesso!");
             }
 
+            AtualizarLista();
+        }
+
+        private static void AtualizarLista()
+        {
             TelaDaLista.DataSource = null;
             TelaDaLista.DataSource = listaReservas;
         }
 
-        private void AoClicarAdicionarAbrirTelaDeCadastro(object sender, EventArgs e)
+        private static bool SomenteUmaLinhaSelecionada()
+        {
+            int qtdLinhasSelecionadas = TelaDaLista.SelectedRows.Count;
+
+            if (qtdLinhasSelecionadas == umaLinhaSelecionada)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool ListaEhNula()
+        {
+            if (listaReservas.Count == listaNula)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static Reserva RetornaReservaSelecionada()
+        {
+            int indexLinha = TelaDaLista.SelectedRows[primeiroElemento].Index;
+            int idLinhaSelecionada = (int)TelaDaLista.Rows[indexLinha].Cells[primeiroElemento].Value;
+            Reserva reservaSelecionada = listaReservas.Find(x => x.Id == idLinhaSelecionada);
+
+            return reservaSelecionada;
+        }
+
+        private void AoClicarAbrirTelaDeCadastro(object sender, EventArgs e)
         {
             Reserva reserva = new();
             TelaCadastroCliente TelaCadastro = new(reserva);
@@ -44,32 +84,40 @@ namespace Sistema_de_Reservas_para_Hoteis
 
         private void AoClicarEditarElementoSelecionado(object sender, EventArgs e)
         {
-            int qtdLinhasSelecionadas = TelaDaLista.SelectedRows.Count;
-
-            if (qtdLinhasSelecionadas == umaLinhaSelecionada)
+            if (ListaEhNula())
             {
-                if (listaReservas.Count == nenhumaLinhaSelecionada)
-                {
-                    MessageBox.Show("Seu programa não possui nenhuma reserva para ser editada.");
-                    return;
-                }
-
-                int indexLinha = TelaDaLista.SelectedRows[primeiroElemento].Index;
-                int idLinhaSelecionada = (int)TelaDaLista.Rows[indexLinha].Cells[primeiroElemento].Value;
-
-                Reserva reservaSelecionada = listaReservas.Find(x => x.Id == idLinhaSelecionada);
-
-                TelaCadastroCliente TelaCadastro = new(reservaSelecionada);
-                TelaCadastro.ShowDialog();
+                MessageBox.Show("Seu programa não possui nenhuma reserva para ser editada.");
             }
-            else if (qtdLinhasSelecionadas > umaLinhaSelecionada)
+            else if (SomenteUmaLinhaSelecionada())
             {
-                MessageBox.Show("Você deve selecionar apenas uma linha para editar.");
+                TelaCadastroCliente TelaCadastro = new(RetornaReservaSelecionada());
+                TelaCadastro.ShowDialog();
             }
             else
             {
-                MessageBox.Show("Você deve selecionar ao menos uma linha para editar.");
+                MessageBox.Show("Selecione uma linha para editar!");
+            }
+        }
 
+        private void AoClicarDeletarElementoSelecionado(object sender, EventArgs e)
+        {
+            if (ListaEhNula())
+            {
+                MessageBox.Show("Seu programa não possui nenhuma reserva para ser deletada.");
+            }
+            else if (SomenteUmaLinhaSelecionada())
+            {
+                string mensagem = $"Você tem certeza que quer deletar a reserva do cliente {RetornaReservaSelecionada().Nome} ?";
+                var deletar = MessageBox.Show(mensagem, "Confirmação de remoção", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (deletar.Equals(DialogResult.Yes))
+                {
+                    listaReservas.Remove(RetornaReservaSelecionada());
+                    AtualizarLista();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma linha para deletar!");
             }
         }
     }
