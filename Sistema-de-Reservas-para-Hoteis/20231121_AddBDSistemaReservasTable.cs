be52b4b232f213f65bcaeb6a64cae23a3ae8679b
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-11-21 10:14:14 -0300

    implementação quickstart fluent migration

---
+++ Sistema-de-Reservas-para-Hoteis/20231121_AddBDSistemaReservasTable.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace Sistema_de_Reservas_para_Hoteis
+{
+    public class AddTabelaReservas : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("TabelaReservas")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Nome").AsString()
+                .WithColumn("Cpf").AsString()
+                .WithColumn("Telefone").AsString()
+                .WithColumn("Idade").AsInt32()
+                .WithColumn("Sexo").AsInt16()
+                .WithColumn("CheckIn").AsDateTime()
+                .WithColumn("CheckOut").AsDateTime()
+                .WithColumn("PrecoEstadia").AsDecimal()
+                .WithColumn("PagamentoEfetuado").AsBoolean();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("TabelaReservas");
+        }
+    }
+}
+}
--- Sistema-de-Reservas-para-Hoteis/Program.cs
+++ Sistema-de-Reservas-para-Hoteis/Program.cs
@@ -1,17 +1,40 @@
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Sistema_de_Reservas_para_Hoteis
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            using (var serviceProvider = CreateServices())
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+
             ApplicationConfiguration.Initialize();
             Application.Run(new TelaListaDeReservas());
+        }
+
+        private static ServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSQLite()
+                    .WithGlobalConnectionString("Data Source=BDSistemaReservas.dbo")
+                    .ScanIn(typeof(AddTabelaReservas).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            runner.MigrateUp();
         }
     }
 }
