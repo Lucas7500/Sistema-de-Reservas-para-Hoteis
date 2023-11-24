47d584b11a860a91f9f1f40ba1e1cd7d8172a662
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-11-24 09:40:41 -0300

    organização do código

--- Sistema-de-Reservas-para-Hoteis/RepositorioBancoDeDados.cs
+++ Sistema-de-Reservas-para-Hoteis/RepositorioBancoDeDados.cs
@@ -6,6 +6,7 @@ namespace Sistema_de_Reservas_para_Hoteis
     internal class RepositorioBancoDeDados : IRepositorio
     {
         private static readonly string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["BDSistemaReservas"].ConnectionString;
+        readonly SqlConnection connection = new (connectionString);
 
         private static Reserva CriarReserva(SqlDataReader leitor)
         {
@@ -26,13 +27,11 @@ namespace Sistema_de_Reservas_para_Hoteis
 
         public List<Reserva> ObterTodos()
         {
-            using SqlConnection connection = new(connectionString);
             connection.Open();
             SqlCommand obterLinhasBD = new("SELECT * FROM TabelaReservas", connection);
             var leitor = obterLinhasBD.ExecuteReader();
-
             List<Reserva> listaReservas = new();
-            
+
             while (leitor.Read())
             {
                 listaReservas.Add(CriarReserva(leitor));
@@ -45,7 +44,6 @@ namespace Sistema_de_Reservas_para_Hoteis
 
         public Reserva ObterPorId(int id)
         {
-            using SqlConnection connection = new(connectionString);
             connection.Open();
             SqlCommand obterObjetoPorId = new($"SELECT * FROM TabelaReservas WHERE Id={id}", connection);
             var leitor = obterObjetoPorId.ExecuteReader();
@@ -55,30 +53,59 @@ namespace Sistema_de_Reservas_para_Hoteis
             {
                 reservaSelecionada = CriarReserva(leitor);
             }
-            
+
+            connection.Close();
+
             return reservaSelecionada;
         }
 
         public void Criar(Reserva reserva)
         {
-            using SqlConnection connection = new(connectionString);
             connection.Open();
-            SqlCommand inserirReservaNaTabela = new($"INSERT INTO TabelaReservas (Nome, Cpf, Telefone, Idade, Sexo, CheckIn, CheckOut, PrecoEstadia, PagamentoEfetuado) VALUES ('{reserva.Nome}', '{reserva.Cpf}', '{reserva.Telefone}', '{reserva.Idade}', '{reserva.Sexo}', '{reserva.CheckIn.Date:dd-MM-yyy}', '{reserva.CheckOut.Date:dd-MM-yyy}', '{reserva.PrecoEstadia.ToString().Replace(',', '.')}', '{reserva.PagamentoEfetuado}')", connection);
+            SqlCommand inserirReservaNaTabela = new($@"
+            INSERT INTO 
+                TabelaReservas (Nome, Cpf, Telefone, Idade, Sexo, CheckIn, CheckOut, PrecoEstadia, PagamentoEfetuado) 
+            VALUES 
+            (
+                '{reserva.Nome}', 
+                '{reserva.Cpf}', 
+                '{reserva.Telefone}', 
+                '{reserva.Idade}', 
+                '{reserva.Sexo}', 
+                '{reserva.CheckIn.Date:dd-MM-yyy}', 
+                '{reserva.CheckOut.Date:dd-MM-yyy}', 
+                '{reserva.PrecoEstadia.ToString().Replace(',', '.')}', 
+                '{reserva.PagamentoEfetuado}'
+            )", connection);
+
             inserirReservaNaTabela.ExecuteReader();
             connection.Close();
         }
         public void Atualizar(Reserva copiaReserva)
         {
-            using SqlConnection connection = new(connectionString);
             connection.Open();
-            SqlCommand editarReservaNaTabela = new($"UPDATE TabelaReservas SET Nome='{copiaReserva.Nome}', Cpf='{copiaReserva.Cpf}', Telefone='{copiaReserva.Telefone}', Idade='{copiaReserva.Idade}', Sexo='{copiaReserva.Sexo}', CheckIn='{copiaReserva.CheckIn.Date:dd-MM-yyyy}', CheckOut='{copiaReserva.CheckOut.Date:dd-MM-yyyy}', PrecoEstadia='{copiaReserva.PrecoEstadia.ToString().Replace(',', '.')}', PagamentoEfetuado='{copiaReserva.PagamentoEfetuado}' WHERE Id={copiaReserva.Id}", connection);
+            SqlCommand editarReservaNaTabela = new($@"
+            UPDATE 
+                TabelaReservas 
+            SET 
+                Nome='{copiaReserva.Nome}', 
+                Cpf='{copiaReserva.Cpf}', 
+                Telefone='{copiaReserva.Telefone}', 
+                Idade='{copiaReserva.Idade}', 
+                Sexo='{copiaReserva.Sexo}', 
+                CheckIn='{copiaReserva.CheckIn.Date:dd-MM-yyyy}', 
+                CheckOut='{copiaReserva.CheckOut.Date:dd-MM-yyyy}', 
+                PrecoEstadia='{copiaReserva.PrecoEstadia.ToString().Replace(',', '.')}', 
+                PagamentoEfetuado='{copiaReserva.PagamentoEfetuado}' 
+                WHERE Id={copiaReserva.Id}
+            ", connection);
+
             editarReservaNaTabela.ExecuteReader();
             connection.Close();
         }
 
         public void Remover(int id)
         {
-            using SqlConnection connection = new(connectionString);
             connection.Open();
             SqlCommand deletarReserva = new($"DELETE FROM TabelaReservas WHERE Id={id}", connection);
             deletarReserva.ExecuteReader();
