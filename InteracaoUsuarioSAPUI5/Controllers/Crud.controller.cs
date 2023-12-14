ce393771909d9954e5a5a4a1801ecfcbc18523b8
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-12-14 14:04:50 -0300

    Crud com controller da Api

---
+++ InteracaoUsuarioSAPUI5/Controllers/Crud.controller.cs
@@ -0,0 +1,62 @@
+﻿using Infraestrutura;
+using Microsoft.AspNetCore.Mvc;
+using FluentValidation;
+using Dominio;
+using Dominio.Extensoes;
+
+namespace InteracaoUsuarioSAPUI5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Crud : ControllerBase
+    {
+        private readonly IRepositorio _repositorio = new RepositorioLinq2DB();
+        private readonly IValidator<Reserva> _validacao = new ReservaFluentValidation();
+
+        [HttpGet]
+        public List<Reserva> ObterTodos()
+        {
+            return _repositorio.ObterTodos();
+        }
+
+        [HttpGet("{id}")]
+        public Reserva ObterPorId(int id)
+        {
+            return _repositorio.ObterPorId(id);
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarReserva([FromBody] Reserva reserva)
+        {
+            if (reserva == null)
+            {
+                return BadRequest();
+            }
+
+            //_validacao.ValidateAndThrowArgumentException(reserva);
+            _repositorio.Criar(reserva);
+            return Created($"reserva/{reserva.Id}", reserva);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarReserva(int id)
+        {
+            var reserva = ObterPorId(id);
+
+            if (reserva == null)
+            {
+                return BadRequest();
+            }
+            _validacao.ValidateAndThrowArgumentException(reserva);
+            _repositorio.Atualizar(reserva);
+            return Created($"reserva/{reserva.Id}", reserva);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult RemoverReserva(int id)
+        {
+            _repositorio.Remover(id);
+            return Ok();
+        }
+    }
+}
