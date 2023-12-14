4dd0b27957f31b905381e220ee00e3e4ba5c5792
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-12-14 09:01:52 -0300

    Codigo para rodar o index.html corretamente até no https

--- InteracaoUsuarioSAPUI5/Program.cs
+++ InteracaoUsuarioSAPUI5/Program.cs
@@ -1,16 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
 
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseStaticFiles();
-}
-
 app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")
+),
+
+    ContentTypeProvider = new FileExtensionContentTypeProvider
+    {
+        Mappings = { [".properties"] = "application/x-msdownload" }
+    }
+});
+
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
