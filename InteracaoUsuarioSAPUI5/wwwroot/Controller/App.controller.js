bf19f469422b3c1ab5d48372afd7ba35e174b7f6
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-12-18 09:54:08 -0300

    Botão na tela, teste

---
+++ InteracaoUsuarioSAPUI5/wwwroot/Controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller"
+], (Controller) => {
+   "use strict";
+
+   return Controller.extend("reservas.hoteis.controller.App", {
+      aoClicarAdicionarReserva() {
+         alert("DEU CERTO PORRAAAAAAAAAAAA");
+      }
+   });
+});
