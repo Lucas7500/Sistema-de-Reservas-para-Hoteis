3aaf02ac17365adb116663825d9edc566b4cd8a9
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-12-19 09:41:54 -0300

    tentativas de utilizar o fetch API

--- InteracaoUsuarioSAPUI5/wwwroot/Component.js
+++ InteracaoUsuarioSAPUI5/wwwroot/Component.js
@@ -2,17 +2,16 @@ sap.ui.define([
     "sap/ui/core/UIComponent"
 ], (UIComponent) => {
     "use strict";
- 
+
     return UIComponent.extend("reservas.hoteis.Component", {
         metadata: {
             interfaces: ["sap.ui.core.IAsyncContentCreation"],
             manifest: "json"
         },
- 
+
         init() {
             UIComponent.prototype.init.apply(this, arguments);
-           this.getRouter().initialize();
+            this.getRouter().initialize();
         }
     });
 });
- 
--- InteracaoUsuarioSAPUI5/wwwroot/Controller/Listagem.controller.js
+++ InteracaoUsuarioSAPUI5/wwwroot/Controller/Listagem.controller.js
@@ -2,11 +2,14 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel"
 ], (Controller, JSONModel) => {
+    "use strict";
+
     return Controller.extend("reservas.hoteis.controller.Listagem", {
         onInit() {
-            fetch("https://localhost:7192/api/Reserva")
-                .then(T => T.json())
-                .then(console.log)
+            fetch('/api/Reserva', { method: 'GET' })
+                .then(response => response.json())
+                .then(response => this.getView().setModel(new JSONModel(response)))
+                .catch(err => console.log(err.message))
         }
     });
 });
