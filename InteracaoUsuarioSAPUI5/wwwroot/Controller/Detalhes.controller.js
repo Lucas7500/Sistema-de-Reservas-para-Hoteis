c3c501df00f43d483cb6f1cf99da717dda25b13f
Lucas Iglesias <lucasiglesias2704@gmail.com>
2023-12-29 16:46:57 -0300

    Facilitando minha vida para a próxima task

--- InteracaoUsuarioSAPUI5/wwwroot/Controller/Detalhes.controller.js
+++ InteracaoUsuarioSAPUI5/wwwroot/Controller/Detalhes.controller.js
@@ -1,12 +1,27 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "../Repositorios/ReservaRepository",
+    "sap/ui/model/json/JSONModel",
     "sap/ui/core/routing/History"
-], (Controller, History) => {
+], (Controller, ReservaRepository, JSONModel, History) => {
     "use strict";
 
     const caminhoRotaDetalhes = "reservas.hoteis.controller.Detalhes";
 
     return Controller.extend(caminhoRotaDetalhes, {
+        onInit() {
+            let rota = this.getOwnerComponent().getRouter();
+            rota.getRoute('detalhes').attachPatternMatched(this._aoCoincidirRota, this);
+        },
+
+        _aoCoincidirRota(oEvent) {
+            let idReserva = oEvent.getParameter("arguments").id;
+            
+            ReservaRepository.obterPorId(idReserva)
+            .then(response => response.json())
+            .then(response => this.getView().setModel(new JSONModel(response)));
+        },
+
         voltarPagina() {
             const oHistory = History.getInstance();
             const sPreviousHash = oHistory.getPreviousHash();
